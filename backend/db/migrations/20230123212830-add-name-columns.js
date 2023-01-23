e8ebe16861e7e1d74f415e2766488ab33f39210f
gracecizma <gracecizma@gmail.com>
2023-01-23 16:22:17 -0600

    updated migration file with options table

--- backend/db/migrations/20230123212830-add-name-columns.js
+++ backend/db/migrations/20230123212830-add-name-columns.js
@@ -1,6 +1,13 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+options.tableName = "Users"
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     /**
@@ -9,10 +16,10 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    await queryInterface.addColumn('Users', 'firstName', {
+    await queryInterface.addColumn(options, 'firstName', {
       type: Sequelize.STRING
     });
-    await queryInterface.addColumn('Users', 'lastName', {
+    await queryInterface.addColumn(options, 'lastName', {
       type: Sequelize.STRING
     });
   },
@@ -24,7 +31,7 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
-    await queryInterface.removeColumn('Users', 'firstName');
-    await queryInterface.removeColumn('Users', 'lastName');
+    await queryInterface.removeColumn(options, 'firstName');
+    await queryInterface.removeColumn(options, 'lastName');
   }
 };
