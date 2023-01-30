c0c27cb695f553838974ccd63cbbe803e66f85b2
gracecizma <gracecizma@gmail.com>
2023-01-29 18:28:13 -0600

    updated

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -25,12 +25,12 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Spot.init({
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER
-    },
+    // id: {
+    //   allowNull: false,
+    //   autoIncrement: true,
+    //   primaryKey: true,
+    //   type: DataTypes.INTEGER
+    // },
     ownerId: {
       type: DataTypes.INTEGER,
       allowNull: false
