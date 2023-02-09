e7b20395d72be3532e6d626446ccc066f4a8f436
gracecizma <gracecizma@gmail.com>
2023-02-09 15:52:29 -0600

    frontend setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,13 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+}
+
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
   // Serve the frontend's index.html file at the root route
@@ -26,12 +33,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-if (process.env.NODE_ENV !== 'production') {
-  router.get('/api/csrf/restore', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
-  });
-}
+
 
 
 // router.get("/api/csrf/restore", (req, res) => {
