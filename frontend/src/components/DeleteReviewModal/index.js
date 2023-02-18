2a6dc7aec8895b769f3dcd6e0080e4237ac10f6a
gracecizma <gracecizma@gmail.com>
2023-02-17 18:58:39 -0600

    delete review button updated

---
+++ frontend/src/components/DeleteReviewModal/index.js
@@ -0,0 +1,40 @@
+import { useDispatch } from "react-redux";
+import { useModal } from "../../context/Modal";
+import { deleteReviewById } from "../../store/reviews";
+
+function DeleteReviewModal({ review }) {
+  const { closeModal } = useModal()
+  const dispatch = useDispatch()
+
+  function refreshPage() {
+    window.location.reload()
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    dispatch(deleteReviewById(review.id)).then(closeModal).then(refreshPage())
+  }
+
+  return (
+    <>
+      <div className="delete-modal">
+        <h1>Confirm Delete</h1>
+        <div>Are you sure you want to delete this review?</div>
+        <div className="confirm-delete">
+          <button
+            className="yes-delete"
+            onClick={handleClick}
+          >
+          </button>
+          <button
+            className="no-delete"
+            onClick={closeModal}
+          >
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default DeleteReviewModal;
